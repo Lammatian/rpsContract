82a3e30256923c19a09ffecd406e9dc803db6b11
Lammatian <mateusz.parafinski@gmail.com>
2018-11-12 21:47:40 +0000

    Modified contract to enhance security and make it more readable

--- rps.sol
+++ rps.sol
@@ -8,14 +8,22 @@ contract rpsContract {
     mapping(address => bool) paid;
     // Count if both players got paid (in terms of a tie)
     uint paidCount;
-    address[2] players;
+    Player[2] players;
+    // 0 - player 0, 1 - player 1, 2 - tie
     uint gameWinner;
     GamePhase gamePhase;
     uint256 gameStake;
     // How many players revealed their choices
     uint revealed;
+
+    struct Player {
+        address add;
+        bool revealed;
+        bool got_paid;
+        uint256 hashed_choice;
+        uint8 choice;
+    }
     
-    enum Shape { Rock, Paper, Scissors }
     // Idle - waiting for new players
     // Started - one player started the game
     // Reveal - waiting for the players to reveal
@@ -28,6 +36,8 @@ contract rpsContract {
         gameStake = 0;
         revealed = 0;
         paidCount = 0;
+        players[0] = Player(0, false, false, 0, 0);
+        players[1] = Player(0, false, false, 0, 0);
     }
 
     /**
@@ -35,83 +45,87 @@ contract rpsContract {
     value of the choice and a random (chosen by the player) seed
      */
     function play(uint256 hashed_choice) public payable returns (string) {
-        require (msg.value >= 10 wei, "Stakes need to be at least 10 Wei");
+        require (gamePhase == GamePhase.Idle || gamePhase == GamePhase.Started, "Game is currently on");
+        require (msg.value >= 1 ether, "Stakes need to be at least 10 Wei");
+        require (msg.value % 2 == 0, "Stakes need to be divisible by 2");
 
-        if (gamePhase == GamePhase.Started) {
-            require (msg.value == gameStake, "Stake needs to be equal to the other player's stake");
-            gamePhase = GamePhase.Reveal;
-            players[1] = msg.sender;
-            paid[msg.sender] = false;
-        } else {
-            gameStake = msg.value;
+        if (gamePhase == GamePhase.Idle) {
+            gameStake = msg.value / 2;
             gamePhase = GamePhase.Started;
-            players[0] = msg.sender;
-            paid[msg.sender] = false;
+            players[0] = Player(msg.sender, false, false, hashed_choice, 0);
+        } else {
+            require (msg.value / 2 == gameStake, "Stake needs to be equal to the other player's stake");
+            gamePhase = GamePhase.Reveal;
+            players[1] = Player(msg.sender, false, false, hashed_choice, 0);
         }
-
-        hashed_choices[msg.sender] = hashed_choice;
 
         return "Ok";
     }
 
-    function reveal(uint256 seed, uint8 choice) public returns (string) {
-        bytes memory reveal_val = abi.encodePacked(seed, choice);
+    function reveal(uint256 nonce, uint8 choice) public returns (string) {
+        require(gamePhase == GamePhase.Reveal, "Game not in reveal phase yet");
+        bytes memory reveal_val = abi.encodePacked(nonce, choice);
+        uint8 p = determinePlayer(msg.sender);
+        require(p != 2, "You are not taking part in the game");
+        require(uint256(keccak256(reveal_val)) == players[p].hashed_choice, "Invalid seed and/or choice");
 
-        if (uint256(keccak256(reveal_val)) != hashed_choices[msg.sender]) {
-            return "Invalid seed and/or choice";
-        } else if (uint256(keccak256(reveal_val)) == hashed_choices[msg.sender]) {
-            choices[msg.sender] = choice;
-            revealed += 1;
+        players[p].choice = choice;
+        players[p].revealed = true;
 
-            if (revealed == 2) {
-                gamePhase = GamePhase.Finished;
-                gameWinner = getWinner(int8(choices[players[0]]), int8(choices[players[1]]));
-            }
-
-            return "Ok";
+        if (players[0].revealed == true && players[1].revealed == true) {
+            gamePhase = GamePhase.Finished;
+            gameWinner = getWinner(int8(players[0].choice), int8(players[1].choice));
         }
     }
 
     function claim() public {
         require(gamePhase == GamePhase.Finished, "Game not finished yet");
+        uint8 p = determinePlayer(msg.sender);
+        require(p != 2, "You are not taking part in the game");
 
-        if (gameWinner == 0 && !paid[msg.sender]) {
-            paid[msg.sender] = true;
+        if (gameWinner == p) {
+            players[p].got_paid = true;
+            msg.sender.transfer(3*gameStake);
+        } else if (gameWinner != p && gameWinner != 2) {
+            players[p].got_paid = true;
             msg.sender.transfer(gameStake);
-            paidCount += 1;
+        } else if (gameWinner == 2) {
+            players[p].got_paid = true;
+            msg.sender.transfer(gameStake);
+        }
 
-            if (paidCount == 2) {
-                reset();
-            }
-        } else if (msg.sender == players[gameWinner - 1]) {
-            msg.sender.transfer(2*gameStake);
+        if (players[0].got_paid && players[1].got_paid) {
             reset();
         }
     }
 
+    function determinePlayer(address add) private view returns(uint8) {
+        if (players[0].add == add) {
+            return 0;
+        } else if (players[1].add == add) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
     function reset() private {
-        delete paid[players[0]];
-        delete paid[players[1]];
-        gameWinner = 0;
         gamePhase = GamePhase.Idle;
-        gameStake = 0;
-        revealed = 0;
-        paidCount = 0;
     }
     
     function getWinner(int8 a, int8 b) public view returns (uint8) {
         require(gamePhase == GamePhase.Finished, "Game not finished yet");
 
         if (a - b == 1) {
-            return 1;
-        } else if (a - b == -1) {
-            return 2;
-        } else if (a - b == 2) {
-            return 2;
-        } else if (a - b == -2) {
-            return 1;
-        } else if (a - b == 0) {
             return 0;
+        } else if (a - b == -1) {
+            return 1;
+        } else if (a - b == 2) {
+            return 1;
+        } else if (a - b == -2) {
+            return 0;
+        } else if (a - b == 0) {
+            return 2;
         }
     }
 
@@ -123,9 +137,9 @@ contract rpsContract {
         } else if (gamePhase == GamePhase.Reveal) {
             return "Waiting for the players to reveal their choices";
         } else if (gamePhase == GamePhase.Finished) {
-            if (gameWinner == 1) {
+            if (gameWinner == 0) {
                 return "Player 1 won, waiting to claim the prize";
-            } else if (gameWinner == 2) {
+            } else if (gameWinner == 1) {
                 return "Player 2 won, waiting to claim the prize";
             } else {
                 return "Tie, waiting for the players to claim the money";
