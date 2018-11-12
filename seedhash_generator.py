57a58baef0590a051f5b78ff0fe9e87209839ded
Lammatian <mateusz.parafinski@gmail.com>
2018-11-12 21:47:09 +0000

    Added hash generator script and seeds for testing

---
+++ seedhash_generator.py
@@ -0,0 +1,9 @@
+from web3 import Web3
+from random import randint
+
+seed = randint(2**128, 2**256)
+choice = int(input("Pick a choice: "))
+
+h = Web3.soliditySha3(['uint256', 'uint8'], [seed, choice])
+print(seed)
+print(int(h.hex(), 16))
