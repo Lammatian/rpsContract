4e22c434cfc5dcd576675662d45b5506089dc5a0
Lammatian <mateusz.parafinski@gmail.com>
2018-11-14 22:13:05 +0000

    Security bug fixes

--- rps.sol
+++ rps.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.16;
 
 contract rpsContract {
 
+    // This should be self-explanatory
     struct Player {
         address add;
         bool revealed;
@@ -42,13 +43,17 @@ contract rpsContract {
         require (msg.value >= 1 ether, "Stakes need to be at least 1 ether");
         require (msg.value % 2 == 0, "Stakes need to be divisible by 2");
 
-        if (gamePhase == GamePhase.Idle) {
+        if (gamePhase == GamePhase.Idle) { // first player starting the game
+            // half of what you put in is treated as deposit
+            // and half as the game prize pool
             gameStake = msg.value / 2;
             gamePhase = GamePhase.Started;
             players[0] = Player(msg.sender, false, false, hashed_choice, 0);
-        } else {
+        } else { // second player joining the game
             require (msg.value / 2 == gameStake, "Stake needs to be equal to the other player's stake");
             gamePhase = GamePhase.Reveal;
+            // start the timer to enable resetting the game
+            // in case of inactivity
             timer = now;
             players[1] = Player(msg.sender, false, false, hashed_choice, 0);
         }
@@ -65,29 +70,35 @@ contract rpsContract {
         players[p].revealed = true;
 
         if (players[0].revealed == true && players[1].revealed == true) {
+            // both players revealed
+            // determine winner and allow claiming winnings
             gamePhase = GamePhase.Finished;
             gameWinner = getWinner(int8(players[0].choice), int8(players[1].choice));
         } else {
+            // start the timer again after activity
             timer = now;
         }
     }
 
     function claim() public {
         if (now - timer > 2 minutes && gamePhase == GamePhase.Reveal) {
+            reset();
+            // 2 minutes have passed since last activity
+            // allow anyone to reset the game
             if (players[0].revealed == true) {
+                // first player revealed, gets all
                 players[0].got_paid = true;
                 players[0].add.transfer(4*gameStake);
-                reset();
             } else if (players[1].revealed == true) {
+                // second player revealed, gets all
                 players[1].got_paid = true;
                 players[1].add.transfer(4*gameStake);
-                reset();
             } else {
+                // no player revealed, split 50/50
                 players[0].got_paid = true;
                 players[1].got_paid = true;
                 players[0].add.transfer(2*gameStake);
                 players[1].add.transfer(2*gameStake);
-                reset();
             }
 
             return;
@@ -96,15 +107,19 @@ contract rpsContract {
         require(gamePhase == GamePhase.Finished, "Game not finished yet");
         uint8 p = determinePlayer(msg.sender);
         require(p != 2, "You are not taking part in the game");
+        require(!players[p].got_paid, "You already have your money");
 
         if (gameWinner == p) {
             players[p].got_paid = true;
+            // transfer 75% of total to winner
             msg.sender.transfer(3*gameStake);
         } else if (gameWinner != p && gameWinner != 2) {
             players[p].got_paid = true;
+            // transfer 25% of total to loser
             msg.sender.transfer(gameStake);
         } else if (gameWinner == 2) {
             players[p].got_paid = true;
+            // transfer each player 50% of total
             msg.sender.transfer(2*gameStake);
         }
 
@@ -130,15 +145,15 @@ contract rpsContract {
     function getWinner(int8 a, int8 b) public view returns (uint8) {
         require(gamePhase == GamePhase.Finished, "Game not finished yet");
 
-        if (a - b == 1) {
+        if ((a - b) % 3 == 1) {
             return 0;
-        } else if (a - b == -1) {
+        } else if ((a - b) % 3 == -1) {
             return 1;
-        } else if (a - b == 2) {
+        } else if ((a - b) % 3 == 2) {
             return 1;
-        } else if (a - b == -2) {
+        } else if ((a - b) % 3 == -2) {
             return 0;
-        } else if (a - b == 0) {
+        } else if ((a - b) % 3 == 0) {
             return 2;
         }
     }
