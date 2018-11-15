110093219c43ed5a2362bb10ca17799f9f50cd7d
Lammatian <mateusz.parafinski@gmail.com>
2018-11-15 00:21:48 +0000

    Prettified seedhash generator

--- seedhash_generator.py
+++ seedhash_generator.py
@@ -4,6 +4,6 @@ from random import randint
 seed = randint(2**128, 2**256)
 choice = int(input("Pick a choice: "))
 
-h = Web3.soliditySha3(['uint256', 'uint8'], [seed, choice])
-print("Seed:", seed)
+h = Web3.soliditySha3(['uint256', 'uint8'], [seed, choice%3])
+print("Seed-choice:", str(seed) + ", " + str(choice%3))
 print("Hash:", int(h.hex(), 16))
